3439dfb1edd8edc8ae229b22de1164e08f09bd37
Huchao <macpro@MyPro.local>
2020-07-25 04:59:17 +0800

    工程初始化

---
+++ Trade/Trade/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Trade
+//
+//  Created by MacPro on 2020/7/2.
+//  Copyright © 2020 MacPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Trade/Trade/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trade
+//
+//  Created by MacPro on 2020/7/2.
+//  Copyright © 2020 MacPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
